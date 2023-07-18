f9a0ca78e1e68c438ff04647e67ca3e97f0c608d
Botan Abdullah <botan.b.abdullah@gmail.com>
2023-07-18 11:14:39 +0300

    difference in weight added

--- oil_gas_logistics/oil_gas_logistics/doctype/loading_unloading_dispatch/loading_unloading_dispatch.js
+++ oil_gas_logistics/oil_gas_logistics/doctype/loading_unloading_dispatch/loading_unloading_dispatch.js
@@ -89,14 +89,44 @@
 
 
 //first version
-frappe.ui.form.on('Loading Unloading Dispatch', {
-    truck_weight(frm) {
-        calculate_product_net_weight(frm);
-    },
-    total_weight_of_truck_and_product(frm) {
-        calculate_product_net_weight(frm);
-    },
+// frappe.ui.form.on('Loading Unloading Dispatch', {
+//     truck_weight(frm) {
+//         calculate_product_net_weight(frm);
+//     },
+//     total_weight_of_truck_and_product(frm) {
+//         calculate_product_net_weight(frm);
+//     },
     
+// });
+
+// function calculate_product_net_weight(frm) {
+//     const truck_weight = frm.doc.truck_weight || 0;
+//     const total_weight_of_truck_and_product = frm.doc.total_weight_of_truck_and_product || 0;
+    
+//     const product_net_weight = total_weight_of_truck_and_product - truck_weight;
+//     const difference_weight = kirkuk_total_net_weight - product_net_weight;
+    
+//     frm.set_value('product_net_weight', product_net_weight);
+//     frm.set_value('difference_weight', difference_weight);
+//     frm.refresh_field('product_net_weight');
+//     frm.refresh_field('difference_weight', difference_weight);
+// }
+
+
+//final version with difference 
+frappe.ui.form.on('Loading Unloading Dispatch', {
+    truck_weight: function(frm) {
+        calculate_product_net_weight(frm);
+    },
+    total_weight_of_truck_and_product: function(frm) {
+        calculate_product_net_weight(frm);
+    },
+    kirkuk_net_weight: function(frm) {
+        calculate_difference_in_weight(frm);
+    },
+    transaction_type: function(frm) {
+        toggle_difference_in_weight_visibility(frm);
+    }
 });
 
 function calculate_product_net_weight(frm) {
@@ -104,7 +134,32 @@ function calculate_product_net_weight(frm) {
     const total_weight_of_truck_and_product = frm.doc.total_weight_of_truck_and_product || 0;
     
     const product_net_weight = total_weight_of_truck_and_product - truck_weight;
-    
+
     frm.set_value('product_net_weight', product_net_weight);
-    frm.refresh_field('product_net_weight');
+}
+
+function calculate_difference_in_weight(frm) {
+    const product_net_weight = frm.doc.product_net_weight || 0;
+    const kirkuk_net_weight = frm.doc.kirkuk_net_weight || 0;
+
+    const difference_in_weight = product_net_weight - kirkuk_net_weight;
+
+    frm.set_value('difference_in_weight', difference_in_weight);
+}
+
+function toggle_difference_in_weight_visibility(frm) {
+    const transactionType = frm.doc.transaction_type;
+
+    if (transactionType === 'IN') {
+        cur_frm.toggle_display('difference_in_weight', true);
+        cur_frm.toggle_display('kirkuk_net_weight', true);
+        cur_frm.toggle_display('cmr', true);
+    } else {
+        cur_frm.toggle_display('difference_in_weight', false);
+        cur_frm.toggle_display('kirkuk_net_weight', false);
+        cur_frm.toggle_display('cmr', false);
+        frm.set_value('difference_in_weight', null);
+        frm.set_value('kirkuk_net_weight', null);
+        frm.set_value('cmr', null);
+    }
 }
