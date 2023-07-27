299d7e0e9cbcd6c0c910a3c5be6f3e33c1b8c20e
Botan Abdullah <botan.b.abdullah@gmail.com>
2023-07-27 15:47:32 +0300

    all fields are calculated

--- oil_gas_logistics/oil_gas_logistics/doctype/loading_unloading_dispatch/loading_unloading_dispatch.js
+++ oil_gas_logistics/oil_gas_logistics/doctype/loading_unloading_dispatch/loading_unloading_dispatch.js
@@ -116,6 +116,124 @@
 // }
 
 
+///////////////////////////////////////////////////////////////////////////// with refresh function
+
+// frappe.ui.form.on('Loading Unloading Dispatch', {
+
+
+//     refresh: function(frm){
+    
+//         calculate_product_net_weight(frm);
+    
+    
+//         calculate_product_net_weight(frm);
+    
+//         calculate_difference_in_weight(frm);
+//         calculate_destination_weight_in_tonne(frm);
+//         calculate_destination_weight_in_meters(frm);
+
+//         calculate_net_weight_in_tonne(frm);
+//         calculate_weight_in_meters(frm);
+//         calculate_normal_weight_in_meters(frm);
+
+//         calculate_weight_in_meters(frm);    
+
+//         calculate_normal_weight_in_meters(frm);
+
+//         toggle_difference_in_weight_visibility(frm);
+
+
+//         toggle_difference_in_weight_visibility(frm);
+//         calculate_destination_weight_in_meters(frm);
+
+
+// }
+// });
+
+// function calculate_product_net_weight(frm) {
+//     const truck_weight = frm.doc.truck_weight || 0;
+//     const total_weight_of_truck_and_product = frm.doc.total_weight_of_truck_and_product || 0;
+    
+//     const product_net_weight = total_weight_of_truck_and_product - truck_weight;
+
+//     frm.set_value('product_net_weight', product_net_weight);
+//     frm.refresh_field('product_net_weight');
+// }
+
+// function calculate_difference_in_weight(frm) {
+//     const product_net_weight = frm.doc.product_net_weight || 0;
+//     const kirkuk_net_weight = frm.doc.kirkuk_net_weight || 0;
+
+//     const difference_in_weight = product_net_weight - kirkuk_net_weight;
+
+//     frm.set_value('difference_in_weight', difference_in_weight);
+//     frm.refresh_field('difference_in_weight');
+// }
+
+// function calculate_net_weight_in_tonne(frm) {
+//     const product_net_weight = frm.doc.product_net_weight || 0;
+//     const net_weight_in_tonne = product_net_weight / 1000;
+//     frm.set_value('net_weight_in_tonne', net_weight_in_tonne);
+//     frm.refresh_field('net_weight_in_tonne');
+// }
+
+// function calculate_destination_weight_in_tonne(frm) {
+//     const kirkuk_net_weight = frm.doc.kirkuk_net_weight || 0;
+//     const destination_weight_in_tonne = kirkuk_net_weight / 1000;
+//     frm.set_value('destination_weight_in_tonne', destination_weight_in_tonne);
+//     frm.refresh_field('destination_weight_in_tonne');
+// }
+
+// function calculate_destination_weight_in_meters(frm) {
+//     const kirkuk_net_weight = frm.doc.kirkuk_net_weight || 0;
+//     const kirkuk_standard_density = frm.doc.kirkuk_standard_density || 0;
+    
+//     const destination_weight_in_meters = (kirkuk_net_weight / 1000) / kirkuk_standard_density;
+    
+//     frm.set_value('destination_weight_in_meters', destination_weight_in_meters);
+//     frm.refresh_field('destination_weight_in_meters');
+// }
+
+// function calculate_weight_in_meters(frm) {
+//     const product_net_weight = frm.doc.product_net_weight || 0;
+//     const suli_standard_density = frm.doc.suli_standard_density || 0;
+    
+//     const weight_in_meters = (product_net_weight / 1000) / suli_standard_density;
+    
+//     frm.set_value('weight_in_meters', weight_in_meters);
+//     frm.refresh_field('weight_in_meters');
+// }
+
+// function calculate_normal_weight_in_meters(frm) {
+//     const product_net_weight = frm.doc.product_net_weight || 0;
+//     const density = frm.doc.density || 0;
+    
+//     const normal_weight_in_meters = (product_net_weight / 1000) / density;
+    
+//     frm.set_value('normal_weight_in_meters', normal_weight_in_meters);
+//     frm.refresh_field('normal_weight_in_meters');
+// }
+
+// function toggle_difference_in_weight_visibility(frm) {
+//     const transactionType = frm.doc.transaction_type;
+
+//     if (transactionType === 'IN') {
+//         cur_frm.toggle_display('difference_in_weight', true);
+//         cur_frm.toggle_display('kirkuk_net_weight', true);
+//         cur_frm.toggle_display('cmr', true);
+//     } else {
+//         cur_frm.toggle_display('difference_in_weight', false);
+//         cur_frm.toggle_display('kirkuk_net_weight', false);
+//         cur_frm.toggle_display('cmr', false);
+//         frm.set_value('difference_in_weight', null);
+//         frm.set_value('kirkuk_net_weight', null);
+//         frm.set_value('cmr', null);
+//     }
+// }
+
+
+
+
 frappe.ui.form.on('Loading Unloading Dispatch', {
     truck_weight: function(frm) {
         calculate_product_net_weight(frm);
@@ -129,6 +247,7 @@ frappe.ui.form.on('Loading Unloading Dispatch', {
     },
     kirkuk_standard_density: function(frm) {
         calculate_destination_weight_in_meters(frm);
+        calculate_normal_weight_in_meters(frm);
     },
     product_net_weight: function(frm) {
         calculate_net_weight_in_tonne(frm);
@@ -138,6 +257,7 @@ frappe.ui.form.on('Loading Unloading Dispatch', {
     },
     weight_in_meters: function(frm) {
         calculate_weight_in_meters(frm); // Recalculate weight_in_meters
+        calculate_normal_weight_in_meters(frm);
     },
     standard_weight_in_meters: function(frm) {
         calculate_normal_weight_in_meters(frm); // Recalculate normal_weight_in_meters
--- oil_gas_logistics/oil_gas_logistics/doctype/loading_unloading_dispatch/loading_unloading_dispatch.py
+++ oil_gas_logistics/oil_gas_logistics/doctype/loading_unloading_dispatch/loading_unloading_dispatch.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2023, botan.b.abdullah@gmail.com and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class LoadingUnloadingDispatch(Document):
@@ -14,3 +14,39 @@ class LoadingUnloadingDispatch(Document):
             self.name = f"Transfer-{self.wb_id}-{self.loading_location}-{self.unloading_location}"
         else:
             frappe.throw('Invalid transaction type selected.')
+
+
+# custom_app.custom_app.doctype.loading_unloading_dispatch.loading_unloading_dispatch.py
+
+    # def calculate_product_net_weight(self):
+    #     truck_weight = self.truck_weight or 0
+    #     total_weight_of_truck_and_product = self.total_weight_of_truck_and_product or 0
+    #     self.product_net_weight = total_weight_of_truck_and_product - truck_weight
+
+    # def calculate_difference_in_weight(self):
+    #     product_net_weight = self.product_net_weight or 0
+    #     kirkuk_net_weight = self.kirkuk_net_weight or 0
+    #     self.difference_in_weight = product_net_weight - kirkuk_net_weight
+
+    # def calculate_net_weight_in_tonne(self):
+    #     product_net_weight = self.product_net_weight or 0
+    #     self.net_weight_in_tonne = product_net_weight / 1000
+
+    # def calculate_destination_weight_in_tonne(self):
+    #     kirkuk_net_weight = self.kirkuk_net_weight or 0
+    #     self.destination_weight_in_tonne = kirkuk_net_weight / 1000
+
+    # def calculate_destination_weight_in_meters(self):
+    #     kirkuk_net_weight = self.kirkuk_net_weight or 0
+    #     kirkuk_standard_density = self.kirkuk_standard_density or 1
+    #     self.destination_weight_in_meters = (kirkuk_net_weight / 1000) / kirkuk_standard_density
+
+    # def calculate_weight_in_meters(self):
+    #     product_net_weight = self.product_net_weight or 0
+    #     suli_standard_density = self.suli_standard_density or 1
+    #     self.weight_in_meters = (product_net_weight / 1000) / suli_standard_density
+
+    # def calculate_normal_weight_in_meters(self):
+    #     product_net_weight = self.product_net_weight or 0
+    #     density = self.density or 1
+    #     self.normal_weight_in_meters = (product_net_weight / 1000) / density
