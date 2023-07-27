16699d10d5213081dbfbc872d5bc4020a5a28cb9
Botan Abdullah <botan.b.abdullah@gmail.com>
2023-07-27 09:35:19 +0300

    fixing differnce in weight

--- oil_gas_logistics/oil_gas_logistics/doctype/loading_unloading_dispatch/loading_unloading_dispatch.js
+++ oil_gas_logistics/oil_gas_logistics/doctype/loading_unloading_dispatch/loading_unloading_dispatch.js
@@ -123,28 +123,24 @@ frappe.ui.form.on('Loading Unloading Dispatch', {
     total_weight_of_truck_and_product: function(frm) {
         calculate_product_net_weight(frm);
     },
-
-    difference_in_weight: function(frm){
-        calculate_difference_in_weight(frm);
-        frm.refresh_field()
-    },
-
     kirkuk_net_weight: function(frm) {
-        //frm.trigger('calculate_difference_in_weight');
+        calculate_difference_in_weight(frm);
         calculate_destination_weight_in_tonne(frm);
-        calculate_destination_weight_in_meters(frm);
     },
     kirkuk_standard_density: function(frm) {
         calculate_destination_weight_in_meters(frm);
     },
     product_net_weight: function(frm) {
         calculate_net_weight_in_tonne(frm);
-        frm.trigger('calculate_weight_in_meters');
-        frm.trigger('calculate_normal_weight_in_meters');
+        calculate_difference_in_weight(frm); // Recalculate difference_in_weight
+        calculate_weight_in_meters(frm);
+        calculate_normal_weight_in_meters(frm);
     },
-    density: function(frm) {
-        frm.trigger('calculate_weight_in_meters');
-        frm.trigger('calculate_normal_weight_in_meters');
+    weight_in_meters: function(frm) {
+        calculate_weight_in_meters(frm); // Recalculate weight_in_meters
+    },
+    standard_weight_in_meters: function(frm) {
+        calculate_normal_weight_in_meters(frm); // Recalculate normal_weight_in_meters
     },
     transaction_type: function(frm) {
         toggle_difference_in_weight_visibility(frm);
@@ -165,15 +161,11 @@ function calculate_difference_in_weight(frm) {
     const product_net_weight = frm.doc.product_net_weight || 0;
     const kirkuk_net_weight = frm.doc.kirkuk_net_weight || 0;
 
-    if (product_net_weight > 0 && kirkuk_net_weight > 0) {
-        const difference_in_weight = product_net_weight - kirkuk_net_weight;
-        frm.set_value('difference_in_weight', difference_in_weight);
-        frm.refresh_field('difference_in_weight');
-    } else {
-        frm.set_value('difference_in_weight', 0);
-    }
-}
+    const difference_in_weight = product_net_weight - kirkuk_net_weight;
 
+    frm.set_value('difference_in_weight', difference_in_weight);
+    frm.refresh_field('difference_in_weight');
+}
 
 function calculate_net_weight_in_tonne(frm) {
     const product_net_weight = frm.doc.product_net_weight || 0;
@@ -199,42 +191,37 @@ function calculate_destination_weight_in_meters(frm) {
     frm.refresh_field('destination_weight_in_meters');
 }
 
-frappe.ui.form.on('Loading Unloading Dispatch', {
-    calculate_weight_in_meters: function(frm) {
-        const product_net_weight = frm.doc.product_net_weight || 0;
-        const suli_standard_density = frm.doc.suli_standard_density || 0;
-        
-        const weight_in_meters = (product_net_weight / 1000) / suli_standard_density;
-        
-        frm.set_value('weight_in_meters', weight_in_meters);
-        frm.refresh_field('weight_in_meters');
-    },
-    calculate_normal_weight_in_meters: function(frm) {
-        const product_net_weight = frm.doc.product_net_weight || 0;
-        const density = frm.doc.density || 0;
-        
-        const normal_weight_in_meters = (product_net_weight / 1000) / density;
-        
-        frm.set_value('normal_weight_in_meters', normal_weight_in_meters);
-        frm.refresh_field('normal_weight_in_meters');
-    }
-});
+function calculate_weight_in_meters(frm) {
+    const product_net_weight = frm.doc.product_net_weight || 0;
+    const suli_standard_density = frm.doc.suli_standard_density || 0;
+    
+    const weight_in_meters = (product_net_weight / 1000) / suli_standard_density;
+    
+    frm.set_value('weight_in_meters', weight_in_meters);
+    frm.refresh_field('weight_in_meters');
+}
+
+function calculate_normal_weight_in_meters(frm) {
+    const product_net_weight = frm.doc.product_net_weight || 0;
+    const density = frm.doc.density || 0;
+    
+    const normal_weight_in_meters = (product_net_weight / 1000) / density;
+    
+    frm.set_value('normal_weight_in_meters', normal_weight_in_meters);
+    frm.refresh_field('normal_weight_in_meters');
+}
 
 function toggle_difference_in_weight_visibility(frm) {
     const transactionType = frm.doc.transaction_type;
 
     if (transactionType === 'IN') {
-        cur_frm.toggle_display('kirkuk_detail_section_section', true);
         cur_frm.toggle_display('difference_in_weight', true);
         cur_frm.toggle_display('kirkuk_net_weight', true);
         cur_frm.toggle_display('cmr', true);
     } else {
-        cur_frm.toggle_display('kirkuk_detail_section_section', false);
         cur_frm.toggle_display('difference_in_weight', false);
         cur_frm.toggle_display('kirkuk_net_weight', false);
         cur_frm.toggle_display('cmr', false);
-        frm.set_value('difference_in_weight', null);
-        frm.set_value('kirkuk_net_weight', null);
-        frm.set_value('cmr', null);
+        // Leave the difference_in_weight as it is
     }
 }
